3468f14ce11e652e547ca53bea4dcad87d3ba9ac
Taylor Williams <taylor@thewilliams.net>
2019-02-16 01:53:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
     if (key === code[index]) {
       index++;
 
-      if (index === alphabet.length) {
+      if (index === code.length) {
         alert("Hurray!");
 
         index = 0;
