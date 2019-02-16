cc2b6a5c0bb660455412b7ea730945d8ac21b6ec
Taylor Williams <taylor@thewilliams.net>
2019-02-16 01:50:40 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 function init() {
   // Write your JavaScript code inside the init() function
   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-  const input = document.addEventListener('keydown', onKeyDownHandler(event))
+  const input = document.querySelector('input')
+  input.addEventListener('keydown', onKeyDownHandler(event))
 
   // Keep track of index outside of the event handler.
   let index = 0;
@@ -22,5 +23,4 @@ function init() {
       index = 0;
     }
   }
-
 }
