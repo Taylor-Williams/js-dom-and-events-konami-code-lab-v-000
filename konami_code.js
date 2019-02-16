01324c12e9b51fffee3392da9d16768b55662559
Taylor Williams <taylor@thewilliams.net>
2019-02-16 01:52:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,14 +1,8 @@
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
-  // Write your JavaScript code inside the init() function
-  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-  const input = document.querySelector('input')
-  input.addEventListener('keydown', onKeyDownHandler(event))
-
-  // Keep track of index outside of the event handler.
   let index = 0;
-
-  // This is the function that would be invoked by the event listener.
-  function onKeyDownHandler(e) {
+  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
     if (key === code[index]) {
@@ -22,5 +16,5 @@ function init() {
     } else {
       index = 0;
     }
-  }
+  })
 }
